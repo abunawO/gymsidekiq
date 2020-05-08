b9ea35c7142e83334eed572c1c7d29aa0d821c29
ajmakhl <ajmakhl@MacBook-Pro.local>
2020-05-08 17:48:11 -0400

    started a layout for app

---
+++ frontend/app/components/sidebar.js
@@ -0,0 +1,10 @@
+import Ember from "ember";
+export default Ember.Component.extend({
+  data: [
+    { title: "profile" },
+    { title: "classes" },
+    { title: "members" },
+    { title: "trainers" },
+  ],
+  actions: {},
+});
