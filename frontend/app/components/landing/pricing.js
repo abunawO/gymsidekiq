f4aa604cd7c84863f0193acb38739a0ce901f172
ajmakhl <ajmakhl@MacBook-Pro.local>
2020-05-08 15:28:39 -0400

    redesigned landing

---
+++ frontend/app/components/landing/pricing.js
@@ -0,0 +1,21 @@
+import Ember from "ember";
+export default Ember.Component.extend({
+  data: [
+    {
+      title: "Free",
+      price: "0",
+      list: ["this", "that", "those", "them"],
+    },
+    {
+      title: "Basic",
+      price: "13",
+      list: ["Everything in Free Plan", "that", "those", "them"],
+    },
+    {
+      title: "Business",
+      price: "39",
+      list: ["Everything in Basic plan", "this", "that", "those", "them"],
+    },
+  ],
+  actions: {},
+});
