cfb56ae6e301e3122944dcc8f05d55a3c227dcbd
ajmakhl <ajmakhl@MacBook-Pro.local>
2020-05-12 14:23:34 -0400

    fixed navbar

--- frontend/app/components/layout.js
+++ frontend/app/components/layout.js
@@ -2,7 +2,33 @@ import Ember from "ember";
 export default Ember.Component.extend({
   store: Ember.inject.service(),
   session: Ember.inject.service(),
-  name: "",
+  profileName: null,
+  init() {
+    this._super(...arguments);
+    this.set("errors", []);
+    let queryParams = {
+      where: {
+        id: {
+          value: this.get("session.data.authenticated.id"),
+          operator: "==",
+        },
+      },
+    };
+    let promises = {
+      user: this.store.query("user", queryParams),
+    };
+    return Ember.RSVP.hash(promises).then((user) => {
+      // debugger;
+      const myprofile = this.store
+        .find("profile", user.user.firstObject.profileId)
+        .then((profile) => {
+          // debugger;
+          this.set("profileName", profile.profileName);
+        });
+      // this.set("profileId", user.user.firstObject.profileId);
+    });
+  },
+
   sidebarLinks: [
     { title: "Profile", to: "user.profile.index" },
     { title: "Plans", to: "user.profile.plans" },
@@ -10,14 +36,5 @@ export default Ember.Component.extend({
     { title: "Trainers", to: "user.profile.trainers" },
     { title: "Members", to: "user.profile.members" },
   ],
-  actions: {
-    getProfileName() {
-      this.store
-        .find("profile", this.get("session.data.authenticated.id"))
-        .then((user) => {
-          this.name = user.profileName;
-        });
-      return this.name;
-    },
-  },
+  actions: {},
 });
