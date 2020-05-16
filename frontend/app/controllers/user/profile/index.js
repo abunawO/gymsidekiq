9a615d552574e8a532aa055ec7fe9be2d7b9a84a
Abunaw Ose <abunawose@gmail.com>
2020-05-16 16:17:49 -0400

    WIP

--- frontend/app/controllers/user/profile/index.js
+++ frontend/app/controllers/user/profile/index.js
@@ -3,9 +3,5 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
   userProfiles: [],
-  actions: {
-    createProfile() {
-      this.transitionToRoute("user.profile.new");
-    }
-  }
+  actions: {}
 });
